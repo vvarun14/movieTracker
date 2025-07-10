02ecd9ee3473f5157a813a29b5c03e2f89528567
vvarun14 <varunsai1411@gmail.com>
2025-07-10 23:34:03 +0530

    Third commit - services and api

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,17 +6,20 @@ function MovieCard({ movie }) {
   }
   return (
     <div className="movie-card">
-      <div className="movie-posrter">
-        <img src={movie.url} alt={movie.title} />
+      <div className="movie-poster">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
         <div className="movie-overlay">
           <button className="favorite-btn" onClick={onFavoriteClick}>
-            🤍
+            ♥
           </button>
         </div>
       </div>
       <div className="movie-info">
         <h3>{movie.title}</h3>
-        <p>{movie.releaseDate}</p>
+        <p>{movie.release_date?.split("-")[0]}</p>
       </div>
     </div>
   );
